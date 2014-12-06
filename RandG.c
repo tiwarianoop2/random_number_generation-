c3a354d50114dfef28f16e4ca4d3a5f5256b6e39
Anoop Tiwari <tiwarianoop2@gmail.com>
2014-12-06 08:23:34 +0530

    Create RandG.c

---
+++ RandG.c
@@ -0,0 +1,12 @@
+//Every time we compile and run we get new memory location on heap.
+//Typecast pointer to int value.
+//Value return is very large so you can use other operators like "%" to reduce it to smalll value.
+
+#include<stdlib.h>
+int RandG()
+{
+int *ptr,rand;
+ptr=(int*)malloc(0);
+rand=(int)ptr;
+return rand;
+}
